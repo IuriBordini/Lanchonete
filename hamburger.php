78463533b83999a6fa538230379f92731e6e780f
Aluno <Aluno@DSN193904>
2018-01-19 21:20:17 -0200

    ok

--- hamburger.php
+++ hamburger.php
@@ -71,7 +71,7 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
 					<li>
-                        <a class="page-scroll" href="index.html">Home</a>
+                                            <a class="page-scroll" href="index.php">Home</a>
                     </li>
 					<li>
                        
