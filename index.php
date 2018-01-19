b6bccb5b2f620c72982f222b3556fdfd9c310902
Aluno <Aluno@DSN194159>
2018-01-19 21:41:12 -0200

    login

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="../Lanchonete/css/estilo.css" rel="stylesheet" type="text/css">
         <meta name="description" content="Free Bootstrap Themes designed by 365bootstrap.com" />
         <meta name="author" content="www.365bootstrap.com" />
         <title>Umbrella</title>
@@ -70,27 +71,65 @@
                     </a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a class="page-scroll" href="index.php">Home</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#services">Services</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#menu">Menu</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#staff">Nosso Chef</a>
-                        </li>
-                        <li>
-
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#booking">Fale conosco</a>
-                        </li>
-
-                    </ul>
+                  <ul class="nav navbar-nav navbar-right">
+					<li>
+                        <a class="page-scroll" href="index.html">Home</a>
+                    </li>
+					<li>
+                        <a class="page-scroll" href="#services">Sobre Nós</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#menu">Menu</a>
+                    </li>
+                   
+					<li>
+                        
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#booking">Fale conosco</a>
+                    </li>
+                    
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+			<ul id="login-dp" class="dropdown-menu">
+				<li>
+					 <div class="row">
+							<div class="col-md-12">
+								Login via
+								<div class="social-buttons">
+									<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+									<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+								</div>
+                                or
+								 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+										<div class="form-group">
+											 <label class="sr-only" for="exampleInputEmail2">Email</label>
+											 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+										</div>
+										<div class="form-group">
+											 <label class="sr-only" for="exampleInputPassword2">Senha</label>
+											 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                             <div class="help-block text-right"><a href="">Esqueceu Senha ?</a></div>
+										</div>
+										<div class="form-group">
+											 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+										</div>
+										<div class="checkbox">
+											 <label>
+											 <input type="checkbox"> keep me logged-in
+											 </label>
+										</div>
+								 </form>
+							</div>
+							<div class="bottom text-center">
+								New here ? <a href="#"><b>Join Us</b></a>
+							</div>
+					 </div>
+				</li>
+			</ul>
+        </li>
+                   
+				</ul>
                 </div>
             </div>
         </nav>
